d7d5a211a79d61d28b1cfc2f0f54154a5ded64cc
Korotaev Boris <korotaev@multivi.ru>
2014-05-28 13:55:30 +0600

    promise task implementation

--- gwynt-core/src/main/java/io/gwynt/core/concurrent/PromiseTask.java
+++ gwynt-core/src/main/java/io/gwynt/core/concurrent/PromiseTask.java
@@ -7,20 +7,83 @@ public class PromiseTask<V> extends DefaultPromise<V> implements RunnableFuture<
 
     protected final Callable<V> task;
 
-    public PromiseTask(Callable<V> task) {
+    PromiseTask(EventExecutor eventExecutor, V result, Runnable task) {
+        this(eventExecutor, toCallable(result, task));
+    }
+
+    PromiseTask(V result, Runnable task) {
+        this(toCallable(result, task));
+    }
+
+    PromiseTask(Callable<V> task) {
         this(null, task);
     }
 
-    public PromiseTask(EventExecutor eventExecutor, Callable<V> task) {
+    PromiseTask(EventExecutor eventExecutor, Callable<V> task) {
         super(eventExecutor);
         this.task = task;
+    }
+
+    static <V> Callable<V> toCallable(V result, Runnable task) {
+        return new RunnableAdapter<>(result, task);
     }
 
     @Override
     public void run() {
         try {
-            task.call();
-        } catch (Exception ignore) {
+            if (!isCancelled()) {
+                V result = task.call();
+                setSuccessInternal(result);
+            }
+        } catch (Throwable e) {
+            setFailureInternal(e);
+        }
+    }
+
+    @Override
+    public boolean trySuccess(V result) {
+        return false;
+    }
+
+    @Override
+    public Promise<V> setFailure(Throwable cause) {
+        throw new IllegalStateException();
+    }
+
+    private PromiseTask<V> setFailureInternal(Throwable cause) {
+        super.setFailure(cause);
+        return this;
+    }
+
+    @Override
+    public boolean tryFailure(Throwable error) {
+        return false;
+    }
+
+    @Override
+    public Promise<V> setSuccess(V result) {
+        throw new IllegalStateException();
+    }
+
+    public PromiseTask<V> setSuccessInternal(V result) {
+        super.setSuccess(result);
+        return this;
+    }
+
+    private static class RunnableAdapter<V> implements Callable<V> {
+
+        private V result;
+        private Runnable task;
+
+        private RunnableAdapter(V result, Runnable task) {
+            this.result = result;
+            this.task = task;
+        }
+
+        @Override
+        public V call() throws Exception {
+            task.run();
+            return result;
         }
     }
 }
