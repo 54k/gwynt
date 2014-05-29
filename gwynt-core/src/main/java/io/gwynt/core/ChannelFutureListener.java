dea0b2edfd563d1e818aefdc44cfef299949b93a
Korotaev Boris <korotaev@multivi.ru>
2014-05-29 16:58:13 +0600

    add close listener

--- gwynt-core/src/main/java/io/gwynt/core/ChannelFutureListener.java
+++ gwynt-core/src/main/java/io/gwynt/core/ChannelFutureListener.java
@@ -3,4 +3,11 @@ package io.gwynt.core;
 import io.gwynt.core.concurrent.FutureListener;
 
 public interface ChannelFutureListener extends FutureListener<ChannelFuture> {
+
+    static ChannelFutureListener CLOSE_LISTENER = new ChannelFutureListener() {
+        @Override
+        public void onComplete(ChannelFuture future) {
+            future.channel().close();
+        }
+    };
 }
