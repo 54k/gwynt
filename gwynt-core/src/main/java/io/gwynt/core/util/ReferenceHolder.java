4e5670fa75ed372df7f159f2e75a1ab6b9aad3d0
Korotaev Boris <korotaev@multivi.ru>
2014-06-23 18:20:37 +0600

    +

---
+++ gwynt-core/src/main/java/io/gwynt/core/util/ReferenceHolder.java
@@ -0,0 +1,87 @@
+package io.gwynt.core.util;
+
+public abstract class ReferenceHolder<T> {
+
+    private final Object lock = new Object();
+    private T ref;
+    private int refs;
+    private final int maxRefs;
+    private short waiters;
+
+    protected ReferenceHolder() {
+        this(Integer.MAX_VALUE);
+    }
+
+    protected ReferenceHolder(int maxRefs) {
+        if (maxRefs <= 0) {
+            throw new IllegalArgumentException("maxRefs > 0");
+        }
+        this.maxRefs = maxRefs;
+    }
+
+    public T acquire() {
+        synchronized (lock) {
+            while (refs == maxRefs) {
+                incWaiters();
+                try {
+                    lock.wait();
+                } catch (InterruptedException ignore) {
+                } finally {
+                    decWaiters();
+                }
+            }
+
+            if (ref == null) {
+                ref = newObject();
+            }
+
+            refs++;
+        }
+
+        return ref;
+    }
+
+    public void release() {
+        if (refs > 0) {
+            throw new IllegalStateException("release() called before acquire()");
+        }
+
+        synchronized (lock) {
+            refs--;
+            if (hasWaiters()) {
+                lock.notifyAll();
+            }
+
+            if (refs == 0) {
+                try {
+                    cleanupObject(ref);
+                } finally {
+                    ref = null;
+                }
+            }
+        }
+    }
+
+    private boolean hasWaiters() {
+        return waiters > 0;
+    }
+
+    private void incWaiters() {
+        if (waiters == Short.MAX_VALUE) {
+            throw new IllegalStateException("Too many waiters");
+        }
+        waiters++;
+    }
+
+    private void decWaiters() {
+        waiters--;
+    }
+
+    public int refCount() {
+        return refs;
+    }
+
+    protected abstract T newObject();
+
+    protected abstract void cleanupObject(T reference);
+}
