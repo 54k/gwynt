5ae0b82136a1cb21ed84d719b560a2656d9fa76b
Korotaev Boris <korotaev@multivi.ru>
2014-07-16 12:39:27 +0600

    +

---
+++ gwynt-core/src/main/java/io/gwynt/core/ChannelOption.java
@@ -0,0 +1,28 @@
+package io.gwynt.core;
+
+import io.gwynt.core.util.AbstractConstant;
+import io.gwynt.core.util.ConstantPool;
+
+public final class ChannelOption<T> extends AbstractConstant<ChannelOption<T>> {
+
+    private static final ConstantPool<ChannelOption<Object>> pool = new ConstantPool<ChannelOption<Object>>() {
+        @Override
+        protected ChannelOption<Object> newConstant(int id, String name) {
+            return new ChannelOption<>(id, name);
+        }
+    };
+
+    private ChannelOption(int id, String name) {
+        super(id, name);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> ChannelOption<T> valueOf(Class<?> firstNameComponent, String secondNameComponent) {
+        return (ChannelOption<T>) pool.valueOf(firstNameComponent, secondNameComponent);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> ChannelOption<T> valueOf(String name) {
+        return (ChannelOption<T>) pool.valueOf(name);
+    }
+}
